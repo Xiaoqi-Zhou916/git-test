7a04d75f7554b474a68183c66937b93108de034a
xiaoqi <342239412@qq.com>
2021-06-17 16:53:38 +0800

    first commit

---
+++ src/main/java/com/xiaoqi/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xiaoqi.git;
+
+/**
+ * @Author:Zhouxq
+ * @Date:Create in 2021/6/17 16:46
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
