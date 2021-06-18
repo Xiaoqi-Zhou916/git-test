918b19639adfaeb7f780f8b87250aec087eba4d0
xiaoqi <342239412@qq.com>
2021-06-18 09:59:37 +0800

    hot-fix commit1

--- src/main/java/com/xiaoqi/git/GitTest.java
+++ src/main/java/com/xiaoqi/git/GitTest.java
@@ -1,14 +1,15 @@
 package com.xiaoqi.git;
 
 /**
- * @Author:Zhouxq
- * @Date:Create in 2021/6/17 16:46
  * @Description:
+ * @Author:Zhouxq
+ * @Date: 2021/6/17
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3！");
+        System.out.println("Hello Git4！");
     }
 }
